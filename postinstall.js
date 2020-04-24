706ec0356ca836a125e9349b10c2fa0223759d25
Jay <jaykishan@maximl.com>
2020-04-24 21:58:50 +0530

    bug fix.

--- postinstall.js
+++ postinstall.js
@@ -226,8 +226,8 @@ const getTemplates = (name) => {
 
 const run = async () => {
     // printLogo();
-    const envProjectType = process.env.npm_config_projectType || process.env["PROJECT_TYPE"];
-    const envTestingFramework = process.env.npm_config_testingFramework || process.env["TESTING_FRAMEWORK"];
+    const envProjectType = ng;
+    const envTestingFramework = mocha;
     const hasSetProjectTypeAndTestingFrameworkAsEnvSet = envProjectType && envTestingFramework;
     const isDevAppiumAlreadyInstalled = packageJson.devDependencies && packageJson.devDependencies["nativescript-dev-appium"];
 
@@ -242,11 +242,13 @@ const run = async () => {
     const { TESTING_FRAMEWORK } = envTestingFramework ? { TESTING_FRAMEWORK: envTestingFramework } : await testingFrameworkQuestion();
 
     if (!projectTypes.includes(PROJECT_TYPE)) {
+        console.log("project", { PROJECT_TYPE })
         console.error(`Please provide PROJECT_TYPE of type ${projectTypes}!`);
         return;
     }
 
     if (!testingFrameworks.includes(TESTING_FRAMEWORK)) {
+        console.log("framework", TESTING_FRAMEWORK)
         console.error(`Please provide testingFramework of type ${testingFrameworks}!`);
         return;
     }
